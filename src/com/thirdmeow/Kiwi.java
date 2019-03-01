01f959a739ac156c6bde3287d854fb854a42d497
The third meow <jsheaandcats@gmail.com>
2019-03-01 21:20:28 +1300

    build out kiwi class

---
+++ src/com/thirdmeow/Kiwi.java
@@ -0,0 +1,43 @@
+package com.thirdmeow;
+
+
+/**
+ * Kiwi is one of the in-game 'characters' or players
+ * It can move around the 2d game environment
+ *
+ * @author John Shea
+ * @version 0.0.1
+ */
+public class Kiwi extends Animal {
+    // Human readable name for kiwi
+    public String name;
+
+    /**
+     * Constructor
+     */
+    Kiwi() {
+        x = 0;
+        y = 0;
+    }
+
+    /**
+     * Constructor with name
+     * @param kiwiName name of kiwi
+     */
+    Kiwi(String kiwiName) {
+        name = kiwiName;
+    }
+
+    /**
+     * Constructor with name and starting position
+     * @param kiwiName name of kiwi
+     * @param start_x starting position on x-axis
+     * @param start_y starting position on y-axis
+     */
+    Kiwi(String kiwiName, int start_x, int start_y) {
+        name = kiwiName;
+        x = start_x;
+        y = start_y;
+    }
+
+}
