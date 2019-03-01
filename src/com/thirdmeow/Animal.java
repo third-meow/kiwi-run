a057ea571f62771fc672469e5c4ef948292c6d2f
The third meow <jsheaandcats@gmail.com>
2019-03-01 21:20:19 +1300

    build out animal class

---
+++ src/com/thirdmeow/Animal.java
@@ -0,0 +1,28 @@
+package com.thirdmeow;
+
+/**
+ * Animal class is base class for all in-game animal entities
+ *
+ * @author John Shea
+ * @version 0.0.1
+ */
+public class Animal {
+    // Position
+    int x, y;
+
+    public void moveUp(int dis) {
+        y += dis;
+    }
+    public void moveDown(int dis) {
+        y -= dis;
+    }
+    public void moveLeft(int dis) {
+        x -= dis;
+
+    }
+    public void moveRight(int dis) {
+        x += dis;
+    }
+
+
+}
