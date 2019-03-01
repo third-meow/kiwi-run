8bee704edf906bbac36e8962596867ab6f7bc733
The third meow <jsheaandcats@gmail.com>
2019-03-01 21:20:47 +1300

    run universe from main

---
+++ src/com/thirdmeow/Main.java
@@ -0,0 +1,15 @@
+package com.thirdmeow;
+
+/**
+ * Simple 2d game
+ *
+ * @author John Shea
+ * @version 0.0.1
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Universe uni = new Universe();
+        uni.run();
+    }
+}
